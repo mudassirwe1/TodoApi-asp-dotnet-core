05f27532271a271f9bfe12fa95e6e5d9d8fe4151
bramiller1989 <bramiller1989@gmail.com>
2021-01-21 11:07:13 -0600

    print to console - test commit

--- Program.cs
+++ Program.cs
@@ -14,6 +14,7 @@ namespace TodoApi
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+            Console.WriteLine("testing");
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
