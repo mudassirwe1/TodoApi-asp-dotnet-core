1f88cb2e90cc1d01657751652f1aa41ff63f69ed
bramiller1989 <bramiller1989@gmail.com>
2021-01-28 11:30:50 -0600

    add getItems method

---
+++ wwwroot/js/site.js
@@ -0,0 +1,10 @@
+﻿const uri = 'api/TodoItems';
+let todos = [];
+
+function getItems() {
+    fetch(uri)
+        .then(response => response.json())
+        .then(data => displayItems(data))
+        .catch(error => console.error('Unable to get items.', error));
+}
+
