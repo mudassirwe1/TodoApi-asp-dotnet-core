752aa56e2b9a728c5d47153caedd795740ff2c60
bramiller1989 <bramiller1989@gmail.com>
2021-01-28 11:46:50 -0600

    add updateItem and closeInput methods

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -48,3 +48,35 @@ function displayEditForm() {
     document.getElementById('edit-isComplete').checked = item.isComplete;
     document.getElementById('editForm').style.display = 'block';
 }
+
+function updateItem() {
+    const item = document.getElementById('edit-id').value;
+    const item = {
+        id: parseInt(itemId, 10),
+        isComplete: document.getElementById('edit-isComplete').checked,
+        name: document.getElementById('edit-name').value.trim()
+    };
+
+    fetch(`${uri}/${itemId}`, {
+        method: 'PUT',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(item)
+    })
+        .then(() => getItems())
+        .catch(error => console.error('Unable to update item.', error));
+
+    closeInput();
+
+    return false;
+}
+
+function closeInput() {
+    document.getElementById('editForm').style.display = 'none';
+}
+
+
+
+
