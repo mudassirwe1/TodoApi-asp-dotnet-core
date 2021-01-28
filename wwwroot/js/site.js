439ac07b4612817b2aacccb3ccd27f541630c65a
bramiller1989 <bramiller1989@gmail.com>
2021-01-28 11:34:50 -0600

    add addItems method

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -8,3 +8,26 @@ function getItems() {
         .catch(error => console.error('Unable to get items.', error));
 }
 
+function addItem() {
+    const addNameTextbox = document.getElementById('add-name');
+
+    const item = {
+        isComplete: false,
+        name: addNameTextbox.value.trim()
+    };
+
+    fetch(uri, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(item)
+    })
+        .then(response => response.json)
+        .then(() => {
+            getItems();
+            addNameTextbox.value = '';
+        })
+        .catch(error => console.error('Unable to add item.', error));
+}
